93765867f5ce8194fca3e8ecc82ea08cc4820167
AndreLucasrs <andrelrs.v@outlook.com>
2019-06-29 16:53:03 -0300

    Navegacao

---
+++ src/pages/categorias/categorias.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the CategoriasPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+// Para gerar um component pelo terminal basta usar o comando
+// ionic g page categorias --- nesse caso foi usado para gerar categorias
+@IonicPage()
+@Component({
+  selector: 'page-categorias',
+  templateUrl: 'categorias.html',
+})
+export class CategoriasPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad CategoriasPage');
+  }
+
+}
