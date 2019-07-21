905b863254d0e79d72cc4d42da88d98ea6629a90
AndreLucasrs <andrelrs.v@outlook.com>
2019-07-20 22:52:36 -0300

    Carregando dados do produto

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -43,8 +43,8 @@ export class ProdutosPage {
     }
   }
 
-  showDatail() {
-    this.navCtrl.push('ProdutoDetailPage');
+  showDatail(produto_id: string) {
+    this.navCtrl.push('ProdutoDetailPage', {produto_id: produto_id});
   }
 
 }
